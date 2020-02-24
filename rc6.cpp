640926da0b8a4f64fd3462958d7b64da2a34637f
doneal1 <doneal1@binghamton.edu>
2020-02-24 14:54:30 -0500

    Update rc6.cpp

--- rc6.cpp
+++ rc6.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 rc6::rc6(string bytes, unsigned int a, unsigned int b, unsigned int c, unsigned int d) {
 	userkey = bytes;
-	byte = userkey.length();
+	byte = userkey.length()/2;
 	A = a;
 	B = b;
 	C = c;
@@ -13,6 +13,57 @@ rc6::rc6(string bytes, unsigned int a, unsigned int b, unsigned int c, unsigned 
 	keys = new unsigned int[44];
 	L = new unsigned int[ceil((float)byte / 4)];
 	
+}
+
+rc6::~rc6() {
+	delete L;
+	delete keys;
+}
+
+unsigned int swapEndian(unsigned int sw) {
+	unsigned int swap = ((sw >> 24) & 0xff) | ((sw << 8) & 0xff0000) | ((sw >> 8) & 0xff00) | ((sw << 24) & 0xff000000);
+	return swap;
+}
+
+string rc6::makeHexString(unsigned int a, unsigned int b, unsigned int c, unsigned int d) {
+	string ret;
+	stringstream add;
+	a = swapEndian(a);
+	for (int i = 7; i > 0; i--) {
+		unsigned int min = (unsigned int)pow(16.0, (double)i);
+		if (a < min) {
+			add << hex << 0;
+		}
+	}	
+	add << hex << a;
+	b = swapEndian(b);
+	for (int i = 7; i > 0; i--) {
+		unsigned int min = (unsigned int)pow(16.0, (double)i);
+		if (b < min) {
+			add << hex << 0;
+		}
+	}
+	add << hex << b;
+	c = swapEndian(c);
+	for (int i = 7; i > 0; i--) {
+		unsigned int min = (unsigned int)pow(16.0, (double)i);
+		if (c < min) {
+			add << hex << 0;
+		}
+	}
+	add << hex << c;
+	d = swapEndian(d);
+	for (int i = 7; i > 0; i--) {
+		unsigned int min = (unsigned int)pow(16.0, (double)i);
+		if (d < min) {
+			add << hex << 0;
+		}
+	}
+	add << hex << d;
+	ret += add.str();
+	add.clear();
+
+	return ret;
 }
 
 unsigned int rc6::rotate(unsigned int a, unsigned int b, int ch) {
@@ -26,20 +77,38 @@ unsigned int rc6::rotate(unsigned int a, unsigned int b, int ch) {
 }
 
 void rc6::keySchedule() {
+	unsigned int add = ceil((float)byte / 4);
+	for (int i = 0; i < (int)add; i++) {
+		unsigned int sw = strtoul(userkey.substr(4 * i, 4).c_str(), 0, 16);
+		sw = swapEndian(sw);
+		L[i] = sw;
+	}
+	keys[0] = p;
+	for (int i = 1; i <= 43; i++) {
+		keys[i] = (keys[i - 1] + q);
+	}
 
+	unsigned int a = 0, b = 0, c = 0, d = 0, f = 3 * max((int)add,44);
+
+	for (int n = 1; n <= (int)f; n++) {
+		a = keys[c] = rotate((keys[c] + a + b), 3, 0);
+		b = L[d] = rotate((L[d] + a + b), a + b, 0);
+		c = (c + 1) % 44;
+		d = (d + 1) % add;
+		
+	}
 }
 
 string rc6::encryption() {
 	string encrypt;
 
 	unsigned int t,u,sw;
-
 	B += keys[0];
 	D += keys[1];
 
 	for (int i = 1; i <= r; i++) {
-		t = rotate(B * (2 * B + 1) % mod, 5, 0);
-		u = rotate(D * (2 * D + 1) % mod, 5, 0);
+		t = rotate(B * (2 * B + 1), 5, 0);
+		u = rotate(D * (2 * D + 1), 5, 0);
 		A = rotate(A ^ t, u, 0) + keys[2 * i];
 		C = rotate(C ^ u, t, 0) + keys[2 * i + 1];
 
@@ -52,6 +121,8 @@ string rc6::encryption() {
 
 	A += keys[42];
 	C += keys[43];
+
+	encrypt = makeHexString(A, B, C, D);
 
 	return encrypt;
 }
@@ -80,6 +151,8 @@ string rc6::decryption() {
 	D -= keys[1];
 	B -= keys[0];
 
+	decrypt = makeHexString(A, B, C, D);
+
 	return decrypt;
 }
 
@@ -96,6 +169,14 @@ string removeSpaces(string text) {
 	return text;
 }
 
+string addSpaces(string text) {
+	string ret = "";
+	for (int i = 0; i < 16; i++) {
+		ret += text.substr(2 * i, 2);
+		ret += " ";
+	}
+	return ret;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -119,16 +200,16 @@ int main(int argc, char* argv[]) {
 	getline(input, text);
 	getline(input, key);
 
-	if(text.compare(0,strlen("plaintext: "), "plaintext: ") == 0)
-		text = text.substr(strlen("plaintext: "), text.length() - strlen("plaintext: "));
-	else if(text.compare(0, strlen("ciphertext: "), "ciphertext: ") == 0)
-		text = text.substr(strlen("ciphertext: "), text.length() - strlen("ciphertext: "));
+	if(text.compare(0,11, "plaintext: ") == 0)
+		text = text.substr(11, text.length() - 11);
+	else if(text.compare(0, 12, "ciphertext: ") == 0)
+		text = text.substr(12, text.length() - 12);
 	else {
 		cout << "Hey....the input file is badly formatted. Do better :(\n";
 		return 1;
 	}
-	if (key.compare(0, strlen("userkey: "), "userkey: ") == 0) {
-		key = key.substr(strlen("userkey: "), key.length() - strlen("userkey: "));
+	if (key.compare(0, 9, "userkey: ") == 0) {
+		key = key.substr(9, key.length() - 9);
 	}
 	else {
 		cout << "Hey....the key is badly formatted. Do better :(\n";
@@ -138,11 +219,36 @@ int main(int argc, char* argv[]) {
 	text = removeSpaces(text);
 	key = removeSpaces(key);
 	
-	unsigned int a = (unsigned int)stoi(text.substr(0,4), 0, 16);
-	unsigned int b = (unsigned int)stoi(text.substr(4, 4), 0, 16);
-	unsigned int c = (unsigned int)stoi(text.substr(8, 4), 0, 16);
-	unsigned int d = (unsigned int)stoi(text.substr(12, 4), 0, 16);
+	unsigned int a = (unsigned int)strtoul(text.substr(0,8).c_str(), 0, 16);
+	unsigned int b = (unsigned int)strtoul(text.substr(8, 8).c_str(), 0, 16);
+	unsigned int c = (unsigned int)strtoul(text.substr(16, 8).c_str(), 0, 16);
+	unsigned int d = (unsigned int)strtoul(text.substr(24, 8).c_str(), 0, 16);
+	a = swapEndian(a);
+	b = swapEndian(b);
+	c = swapEndian(c);
+	d = swapEndian(d);
+
+	rc6 run(key, a, b, c, d);
+	run.keySchedule();
+
+	string out;
+	if(line.compare(0, 10, "Encryption") == 0) {
+		out = run.encryption();
+		out = addSpaces(out);
+		output << "ciphertext: " << out;
+	}
+	else if (line.compare(0, 10, "Decryption") == 0) {
+		out = run.decryption();
+		out = addSpaces(out);
+		output << "plaintext: " << out;
+	}
+	else {
+		cout << "Either Encryption or Decryption please!\n";
+		return 1;
+	}
+
 	
-	rc6(key, a, b, c, d);
+	cout << out << "\n";
+	
 	return 0;
 }
